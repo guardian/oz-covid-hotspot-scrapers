a37b6fe9cd501ae38e7f310cbafcc4860e073071
Josh Nicholas <hi@thambi.li>
2021-06-10 10:22:14 +1000

    Vic scraper

--- vic-hotspots/modules/syncDoc.py
+++ vic-hotspots/modules/syncDoc.py
@@ -6,9 +6,8 @@ AWS_KEY = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_SESSION = os.environ['AWS_SESSION_TOKEN']
 
-
 def syncData(jsonObject,id):
-	
+
 	finalJson = json.dumps(jsonObject, indent=4)
 
 	print("Connecting to S3")
@@ -21,27 +20,26 @@ def syncData(jsonObject,id):
 	)
 	s3 = session.resource('s3')
 
-	key = "yacht-charter-data/{id}.json".format(id=id)
+	key = "docsdata/{id}.json".format(id=id)
 	object = s3.Object(bucket, key)
 	object.put(Body=finalJson, CacheControl="max-age=30", ACL='public-read', ContentType="application/json")
 
 	print("JSON is updated")
 
-	print("data", "https://interactive.guim.co.uk/yacht-charter-data/{id}.json".format(id=id))
-	print("chart", "https://interactive.guim.co.uk/embed/aus/2020/yacht-charter-v15/index.html?key={id}&location=yacht-charter-data".format(id=id))
-	
-def yachtCharter(template, data, chartName, chartId=[{"type":"linechart"}], options=[{"colorScheme":""}],key=[], periods=[], labels=[]):
+	print("data", "https://interactive.guim.co.uk/docsdata/{id}.json".format(id=id))
+
+def syncDoc(template, data, chartName, chartId=[{"type":"linechart"}], options=[],key=[], periods=[], labels=[]):
 
 	jsonDictObject = {
 		"sheets":{
-			"template":template,
+			"details":template,
 			"data":data,
 			"labels":labels,
 			"key":key,
 			"periods":periods,
 			"labels":labels,
 			"chartId":chartId,
-			"options":options,
+			"options":options
 			}
 	}
 
