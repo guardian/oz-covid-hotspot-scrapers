3bb1807094329a26fb408c84d9a9fb1ed0080381
jerseywonder <work@andyball.info>
2021-06-10 15:25:18 +1000

    Remve lambdas

--- public-exposure-sites-in-victoria/contains.js
+++
@@ -1,8 +0,0 @@
-module.exports = function contains(a, b) {
-    // array matches
-    if (Array.isArray(b)) {
-        return b.some(x => a.indexOf(x) > -1);
-    }
-    // string match
-    return a.indexOf(b) > -1;
-}
