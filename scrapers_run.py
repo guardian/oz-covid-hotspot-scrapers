eea74a269e148424637ac097bb13bb3d89b9f078
Josh Nicholas <hi@thambi.li>
2021-06-28 14:47:53 +1000

    New WA scraper

--- scrapers_run.py
+++ scrapers_run.py
@@ -2,3 +2,4 @@
 
 import vic_hotspots_two.vic_hotspot_scraper
 import qld_hotspots.qld_hotspot_scraper
+import wa_hotspots.wa_scraper
--- wa_hotspots/wa_scraper.py
+++ wa_hotspots/wa_scraper.py
@@ -1,7 +1,10 @@
-import requests 
 import pandas as pd 
 import os 
-import time 
+from bs4 import BeautifulSoup as bs
+from modules.yachtCharter import yachtCharter
+
+
+print("Grabbing WA hotspot chart")
 
 here = os.path.dirname(__file__)
 data_path = os.path.dirname(__file__) + "/data/"
@@ -11,19 +14,24 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 urlo = 'https://healthywa.wa.gov.au/Articles/A_E/Coronavirus/Locations-visited-by-confirmed-cases'
-# r = requests.get(urlo)
 
-listo = []
 options = Options()
 options.headless = True
 driver = webdriver.Firefox(options=options)
-# driver = webdriver.Firefox()
 driver.get(urlo)
 
 html = driver.page_source
-tables = pd.read_html(html)
 
-callums = ['Get tested immediately and quarantine for 14 days', 'Get tested immediately, quarantine until a negative result is returned and monitor for symptoms', 'Monitor for symptoms']
+## GRAB THE HEADLINES FROM THE COLLAPSABLE BUTTONS
+soup = bs(html, 'html.parser')
+callums = soup.find_all(class_="doh-accordian_btn")
+callums = [x.text for x in callums]
+callums = [x.replace("Public exposure sites – ", '') for x in callums]
+callums = [x.strip() for x in callums]
+
+callums = [x for x in callums if x.lower() != "flights"]
+
+tables = pd.read_html(html)
 
 listo = []
 
@@ -39,5 +47,40 @@ for i in range(0,len(callums)):
 
 final = pd.concat(listo)
 
-print(final)
-driver.close()
+final['Date'] = pd.to_datetime(final['Date'])
+final['Date'] = final['Date'].dt.strftime('%d/%m/%Y')
+
+# print(final)
+
+print("Making WA hotspot chart")
+
+def makeTable(df):
+	
+    template = [
+            {
+                "title": "Western Australia Covid Hotspots",
+                "subtitle": f"""""",
+                "footnote": "",
+                "source": "Western Australia government",
+                "yScaleType":"",
+                "minY": "0",
+                "maxY": "",
+                "x_axis_cross_y":"",
+                "periodDateFormat":"",
+                "margin-left": "50",
+                "margin-top": "30",
+                "margin-bottom": "20",
+                "margin-right": "10"
+            }
+        ]
+    key = []
+    # labels = []
+    df.fillna("", inplace=True)
+    chartData = df.to_dict('records')
+    labels = []
+
+
+    yachtCharter(template=template, labels=labels, data=chartData, chartId=[{"type":"table"}], 
+    options=[{"colorScheme":"guardian","format": "scrolling","enableSearch": "TRUE","enableSort": "TRUE"}], chartName="wa_covid_hotspots")
+
+makeTable(final)
