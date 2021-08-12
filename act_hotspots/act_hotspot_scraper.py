cab4a4da9d0909ed9337287229533733a416f54b
Josh Nicholas <hi@thambi.li>
2021-08-12 13:10:12 +1000

    ACT hotspots

---
+++ act_hotspots/act_hotspot_scraper.py
@@ -0,0 +1,72 @@
+#%%
+import pandas as pd
+import requests
+from modules.yachtCharter import yachtCharter
+import os
+import re
+
+print("Grabbing ACT hotspot data")
+
+# testo = "_test"
+testo = ''
+
+chart_key = "act_covid_hotspots"
+
+#%%
+
+data_path = os.path.dirname(__file__)
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+headers = {'user-agent': 'The Guardian'}
+html = requests.get('https://www.covid19.act.gov.au/act-status-and-response/act-covid-19-exposure-locations', headers=headers).text
+tables = pd.read_html(html)
+
+table_labels = ['Close contact', 'Monitor for symptoms']
+
+cols = ["Suburb","Place","Date","Arrival time","Departure time"]
+
+listo = []
+
+for i in range(0, len(table_labels)):
+    inter = tables[i]
+    inter = inter[cols]
+    inter['Type'] = table_labels[i]
+    listo.append(inter)
+
+
+df = pd.concat(listo)
+
+print(df)
+
+#%%
+
+def makeTable(df):
+
+    template = [
+            {
+                "title": "ACT Covid Hotspots",
+                "subtitle": f"""""",
+                "footnote": "",
+                "source": "ACT Government",
+                "yScaleType":"",
+                "minY": "0",
+                "maxY": "",
+                "x_axis_cross_y":"",
+                "periodDateFormat":"",
+                "margin-left": "50",
+                "margin-top": "30",
+                "margin-bottom": "20",
+                "margin-right": "10"
+            }
+        ]
+    key = []
+    # labels = []
+    df.fillna("", inplace=True)
+    chartData = df.to_dict('records')
+    labels = []
+
+
+    yachtCharter(template=template, labels=labels, data=chartData, chartId=[{"type":"table"}],
+    options=[{"colorScheme":"guardian","format": "scrolling","enableSearch": "TRUE","enableSort": "TRUE"}], chartName=f"{chart_key}{testo}")
+
+makeTable(df)
