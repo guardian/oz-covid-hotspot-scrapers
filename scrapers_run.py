b30107f9d9b6f2698ae503d895c4b4d4eb4a45df
Josh Nicholas <hi@thambi.li>
2021-08-18 17:12:53 +1000

    kill gecko

--- scrapers_run.py
+++ scrapers_run.py
@@ -1,8 +1,10 @@
 
+import os
+os.system("taskkill /im geckodriver.exe /f")
 
 import vic_hotspots_two.vic_hotspot_scraper
 import qld_hotspots.qld_hotspot_scraper_3
-# import act_hotspots.act_hotspot_scraper_2
+import act_hotspots.act_hotspot_scraper_2
 
 try:
     import wa_hotspots.wa_scraper
