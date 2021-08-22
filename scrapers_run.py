8f1a282ac0396603586e0552c3a5eb811e8eba8d
Josh Nicholas <hi@thambi.li>
2021-08-23 09:06:54 +1000

    Stop QLD scraper erroring when empty

--- scrapers_run.py
+++ scrapers_run.py
@@ -1,6 +1,10 @@
 import vic_hotspots_two.vic_hotspot_scraper
-import qld_hotspots.qld_hotspot_scraper_3
 import act_hotspots.act_hotspot_scraper_3
+
+try:
+    import qld_hotspots.qld_hotspot_scraper_3
+except:
+    pass
 
 try:
     import wa_hotspots.wa_scraper
