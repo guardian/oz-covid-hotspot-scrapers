c133c598e43290c46226e4ca8f27d5c62e40f5f8
Josh Nicholas <hi@thambi.li>
2021-11-29 04:25:22 +0530

    Remove scrapers

--- scrapers_run.py
+++ scrapers_run.py
@@ -1,12 +1,12 @@
-import vic_hotspots_two.vic_hotspot_scraper
-import act_hotspots.act_hotspot_scraper_3
+# import vic_hotspots_two.vic_hotspot_scraper
+# import act_hotspots.act_hotspot_scraper_3
 
-try:
-    import qld_hotspots.qld_hotspot_scraper_3
-except:
-    pass
+# try:
+#     import qld_hotspots.qld_hotspot_scraper_3
+# except:
+#     pass
 
-try:
-    import wa_hotspots.wa_scraper
-except:
-    pass
+# try:
+#     import wa_hotspots.wa_scraper
+# except:
+#     pass
